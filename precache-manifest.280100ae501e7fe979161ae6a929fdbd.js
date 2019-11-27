234157e232368584086cf21bd7fa113b5e3b8854
LENA EIVY <applena@gmail.com>
2019-11-26 16:13:02 -0800

    Updates

---
+++ precache-manifest.280100ae501e7fe979161ae6a929fdbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24a4629682b4f785ef3993afb9b8e1c9",
+    "url": "/static/media/flag.24a46296.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "65f3e0e36d44de7f1c71",
+    "url": "/static/js/main.0da36d8b.chunk.js"
+  },
+  {
+    "revision": "944abb93a582502fd1a4",
+    "url": "/static/js/2.d5669982.chunk.js"
+  },
+  {
+    "revision": "65f3e0e36d44de7f1c71",
+    "url": "/static/css/main.1bf39269.chunk.css"
+  },
+  {
+    "revision": "7f297e7f0500c0007e5f025c47ebb02c",
+    "url": "/index.html"
+  }
+];
